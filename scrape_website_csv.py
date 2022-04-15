ff2d4a4a12799456c5300c8690050246a6756ed2
Robert Jones <60453163+rbjones92@users.noreply.github.com>
2022-04-15 13:51:10 -0700

    Update scrape_website_csv.py

--- scrape_website_csv.py
+++ scrape_website_csv.py
@@ -5,24 +5,27 @@
 
 import requests # For DLing HTML
 from bs4 import BeautifulSoup as bs # To work with HTML
-
-DOMAIN = 'https://www1.nyc.gov'
+ 
+# website URL to scrape
 URL = 'https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page'
 FILETYPE = '.csv'
 
-
+# function to parse website
 def get_soup(url):
     return bs(requests.get(url).text, 'html.parser')
 
-
+# find all links
 for link in get_soup(URL).find_all('a'):
+    # look for links within a tags
     csv_link = link.get('href')
+    # if filetype (.csv) is in link then...
     if FILETYPE in csv_link:
         print(csv_link)
         print(link.text)
+        
         '''
+        # save file to hard drive 
         with open(link.text, 'wb') as file:
             response = requests.get(csv_link)
             file.write(response.content)
-            '''
-            
+        '''
